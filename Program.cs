cc4e5a5c04cdf2c4f2400d63e592e370a71eef5a
Gavin <72846934+gav06@users.noreply.github.com>
2020-12-15 10:38:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace first_c__thing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Nice to meet you " + name + "!");
+            Console.WriteLine("The time is currently " + time + "!" );
+            Console.Write("\n\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
